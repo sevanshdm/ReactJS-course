ff2e3fd7fe8508e5774577036797b80b48044d99
sevans <sevans@highdesertmilk.com>
2023-10-19 12:56:50 -0600

    Video 6. Class Components

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -2,16 +2,20 @@
 
 import logo from './logo.svg';
 import './App.css';
+
 //default import(you can name it anything you want, not just Greet.)
 import Greet from './components/Greet'
 //named export, the name has to match what was exported
 //import { Greet } from './components/Greet'
+
+import Welcome from './components/Welcome'
 
 // The App component represents the view which you see in the browser.
 function App() {
   return (
     <div className="App">
       <Greet/>
+      <Welcome/>
     </div>
   );
 }
---
+++ hello-world/src/components/Welcome.js
@@ -0,0 +1,11 @@
+// Class components needs these imports.
+import React, {Component} from 'react'
+
+//Class component
+class Welcome extends Component {
+    render(){
+        return <h1>Class Component</h1>
+    }
+}
+
+export default Welcome

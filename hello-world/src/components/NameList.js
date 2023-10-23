b7fe47aa7314bf7cf7edbbda125a4cb1be4badb3
sevans <sevans@highdesertmilk.com>
2023-10-23 13:52:30 -0600

    Video 19. Index as key anti-pattern

--- hello-world/src/components/NameList.js
+++ hello-world/src/components/NameList.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Person from './Person'
 
 function NameList() {
-    const names = ['Shrek', 'Donkey', 'Gingy']
+    const names = ['Shrek', 'Donkey', 'Gingy', 'Shrek']
 
     const persons = [
         {
@@ -24,9 +24,18 @@ function NameList() {
             skill: 'Baking'
         }
     ]  
-    const personList = persons.map(person => <Person key={person.id} person ={person}/>)
+
+    // arrow funct can take 2 params. (This is how to assign the index as the key for list rendering.)
+    const nameList = names.map((name, index) => <h2 key={index}> {index} {name} </h2>)
+    //Using an index as a key can cause big UI issues, Use as a last resort.
+    //Only use if 1.) the items in your list don't have a unique ID 2.) The list is a static list and won't change 3.) The list will never be reordered or filtered.
     
-    return <div>{personList}</div>
+    return <div>{nameList}</div>
+
+    // Below works if the objects have an ID attribute.
+    // const personList = persons.map(person => <Person key={person.id} person ={person}/>)
+    
+    // return <div>{personList}</div>
 }
 
 export default NameList

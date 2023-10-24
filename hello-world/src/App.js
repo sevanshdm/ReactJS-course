faa610b1262ecffa8d8cdf35b52199775a6d2c8e
sevans <sevans@highdesertmilk.com>
2023-10-24 09:23:51 -0600

    VIdeo 23. Component Mounting Lifecycles

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -24,13 +24,17 @@ import Inline from './components/Inline';
 //import './appStyles.css'
 import styles from './appStyles.module.css'
 import Form from './components/Form';
+import LifecycleA from './components/LifecycleA';
+
 
 // The App component represents the view which you see in the browser.
 function App() {
   return (
     <div className="App">
 
-      <Form/>
+      <LifecycleA/>
+
+      {/* <Form/> */}
 
       {/* <h1 className='error'>Error</h1>
       <h1 className={styles.success}>Success</h1> */}

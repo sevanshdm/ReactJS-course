78cc448bbbaa7d24b77a4ba068fff551d8dea108
sevans <sevans@highdesertmilk.com>
2023-10-25 08:59:10 -0600

    Video 27. Memo

--- hello-world/src/components/ParentComp.js
+++ hello-world/src/components/ParentComp.js
@@ -1,6 +1,7 @@
 import React, { Component, PureComponent } from 'react'
 import RegComp from './RegComp'
 import PureComp from './PureComp'
+import MemoComp from './MemoComp'
 
 
 //This file shows the difference between Reg and Pure Components
@@ -29,8 +30,9 @@ class ParentComp extends Component {
             return (
                 <div>
                     Parent Component
-                    <RegComp name={this.state.name}/>
-                    <PureComp name={this.state.name}/>
+                    <MemoComp name={this.state.name}/>
+                    {/* <RegComp name={this.state.name}/>
+                    <PureComp name={this.state.name}/> */}
                 </div>
             )
     }

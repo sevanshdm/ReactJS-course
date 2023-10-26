7804474fb8ee397df4c8b945e99bd035b00b1332
sevans <sevans@highdesertmilk.com>
2023-10-26 14:27:11 -0600

    Video 43 - HTTP Post Request

--- HTTP/react-http/src/App.js
+++ HTTP/react-http/src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
-
+import PostForm from './components/PostForm';
 import PostList from './components/PostList';
 import axios from 'axios' //Library
 
 function App() {
   return (
     <div className="App">
-      <PostList/>
+      {/* <PostList/> */}
+      <PostForm/>
     </div>
   );
 }

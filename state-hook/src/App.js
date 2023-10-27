7e8c5ff5a32ff029063b10569e6faada7acb791a
sevans <sevans@highdesertmilk.com>
2023-10-27 10:23:57 -0600

    Video 4 - useState with object

--- state-hook/src/App.js
+++ state-hook/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
 
-import HookCounterTwo from './components/HookCounterTwo';
+
+import HookCounterThree from './components/HookCounterThree';
 
 function App() {
   return (
     <div className="App">
-      <HookCounterTwo/>
+      <HookCounterThree/>
     </div>
   );
 }

85e24dbfdc06377f385b0b9382cbfc0121e4e04a
sevans <sevans@highdesertmilk.com>
2023-10-27 14:29:40 -0600

    Video 14 - Fetching Data with useEffect Part 3

--- state-hook/src/components/DataFetching.js
+++ state-hook/src/components/DataFetching.js
@@ -9,8 +9,14 @@ function DataFetching() {
 
     const [id, setId] = useState(1)
 
+    const [idFromButtonClick, setIdFromButtonClick] = useState(1)
+
+    const handleClick = () => {
+        setIdFromButtonClick(id)
+    }
+
     useEffect(() => {                                      //append post id
-        axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`) //this returns a promise
+        axios.get(`https://jsonplaceholder.typicode.com/posts/${idFromButtonClick}`) //this returns a promise
             .then(res => {
                 console.log(res)
                 setPost(res.data)
@@ -18,11 +24,12 @@ function DataFetching() {
             .catch(err => {
                 console.log(err)
             })
-    }, [id]) // empty dependency array prevents an infinite loop of fetching data. [id] for when you want to rerender whenever the id value changes
+    }, [idFromButtonClick]) // empty dependency array prevents an infinite loop of fetching data. [id] for when you want to rerender whenever the id value changes
 
     return (
         <div>
             <input type='text' value={id} onChange={e => setId(e.target.value)}/>
+            <button type='button' onClick={handleClick}>Fetch Post</button>
             <div>{post.title}</div>
             {/* <ul>
                 {           // for each post, return a list item where key = post.id and inner html is post.title

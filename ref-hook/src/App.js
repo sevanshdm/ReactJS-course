59f240422229f096c53ca6034ddf0bc756b0244f
sevans <sevans@highdesertmilk.com>
2023-10-30 15:30:49 -0600

    Video 28

---
+++ ref-hook/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import FocusInput from './components/FocusInput';
+
+function App() {
+  return (
+    <div className="App">
+
+      <FocusInput/>
+
+    </div>
+  );
+}
+
+export default App;
